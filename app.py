b26fe8320e396044e1bd7fd848e5691f38a60dbc
pgayathri511 <pgayathri511@gmail.com>
2026-08-26 15:24:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <html>
+    <head>
+        <title>Pipeline Web App</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f4f6f9;
+                text-align: center;
+                padding: 50px;
+            }
+
+            h1 {
+                color: #0078d4;
+            }
+
+            p {
+                font-size: 20px;
+                color: #333;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to My Pipeline Web App!</h1>
+        <p>This Flask application is deployed using Azure DevOps Pipeline.</p>
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run()
+  
